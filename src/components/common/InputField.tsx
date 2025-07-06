6529b684d45079e977a10b20d9e2b7f1b26550c2
S Joel Franklin <joel.bitsgoa@gmail.com>
2025-07-06 18:27:59 -0500

    make text visible in mobile

--- src/components/common/InputField.tsx
+++ src/components/common/InputField.tsx
@@ -11,7 +11,7 @@ export function InputField({name, text, placeholder, onChange, isPassword}: Prop
 
     return (
         <div>
-            <label htmlFor={name} className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">{text}</label>
+            <label htmlFor={name} className="block mb-2 text-sm font-medium text-gray-900">{text}</label>
             <input type={isPassword ? "password" : "text"} onChange = {onChange} id={name} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-40 md:w-80 p-2.5" placeholder={placeholder} required />
         </div>
     )
